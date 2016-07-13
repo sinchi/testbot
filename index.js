aa4fc308c0d7a6170234555740c7b9176fa23745
ayoub <ayoub.belghar@gmail.com>
2016-07-13 11:04:29 +0100

    wa ti fin

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            sendMessage(event.sender.id, {text: "wa ti fin a : " + event.message.text});
         }
     }
     res.sendStatus(200);
