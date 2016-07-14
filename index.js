ca92312caaabdff9fae4fd3cf20817efd3226010
ayoub <ayoub.belghar@gmail.com>
2016-07-14 10:06:07 +0100

    fata

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function sendMessage(recipientId, message) {
 function fatafeat(recipientId, text){
     sendSeen(recipientId);
     sendEcrire(recipientId);
-    var fatafeat = 'http://www.fatafeat.com/recipes/search?keyword=&category=&season=&chef=&kitchen=&group=#section=&category=&season=&chef=&kitchen=&group=&keyword=&page='+text;
+    var fatafeat = 'http://www.fatafeat.com/recipes/search?section=&category=&season=&chef=&kitchen=&group=&keyword=&page='+text;
     console.log(fatafeat);
     request(fatafeat, function(error, response, html){
       if(!error){
