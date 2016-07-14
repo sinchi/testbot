62517425b065921223212e7cf318ba0b33dc974c
ayoub <ayoub.belghar@gail.com>
2016-07-14 19:02:03 +0100

    greeting remove

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/webhook', function (req, res) {
 
 // handler receiving messages
 app.post('/webhook', function (req, res) {
-    sendGreeting();
+  //  sendGreeting();
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
