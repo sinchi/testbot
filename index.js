607997485f44e20d4bffce0ec8335e8fcfaab093
ayoub <ayoub.belghar@gmail.com>
2016-07-14 16:41:50 +0100

    rihana

--- index.js
+++ index.js
@@ -218,6 +218,8 @@ function rihana(recipientId, text){
       break;
     case "gateau":
         rihanaLink = 'http://chhiwat.ma/gateaux-2/gateaux-et-cakes/page'+ Number(values[1]) + '/';
+    case "pizza":
+      rihanaLink = 'http://chhiwat.ma/recettes-divers/pizza-pastry/page' + Number(values[1]) + '/';
     break;
     default:
 
