77565d67c5a560a583ed844a7a402903e2876d74
ayoub <ayoub.belghar@gmail.com>
2016-07-15 10:14:50 +0100

    payload

--- index.js
+++ index.js
@@ -167,8 +167,8 @@ app.post('/webhook', function (req, res) {
 
             } else if (event.postback) {
               var payload = JSON.stringify(event.postback).split(',');
-              var id = Number(payload[0]);
-              console.log(id+ ' ' + payload[1] + ' ' + payload[2]);            
+              var id = payload[0].split(':');
+              console.log(id[1]+ ' ' + payload[1] + ' ' + payload[2]);            
               //console.log("Postback received: " + JSON.stringify(event.postback));
           }else if(event.message && event.message.is_echo){
             console.log(event.message.metadata);
