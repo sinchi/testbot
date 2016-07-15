9de32ac6d720be5d9f11735859ca930370e5574d
sinchi <ayoub.belghar@gmail.com>
2016-07-15 02:39:16 +0100

    ingredients

--- index.js
+++ index.js
@@ -104,9 +104,9 @@ app.post('/webhook', function (req, res) {
 
             } else if (event.postback) {
               var payload = JSON.stringify(event.postback).split(',');
-              console.log('recipientId :' + payload[0]);
-              console.log(payload[1]);
-              console.log('lien :' + payload[2]);
+              console.log('recipientId :' + payload[1]);
+              console.log(payload[2]);
+              console.log('lien :' + payload[3]);
               //console.log("Postback received: " + JSON.stringify(event.postback));
           }else if(event.message && event.message.is_echo){
             console.log(event.message.metadata);
@@ -356,7 +356,7 @@ function rihana(recipientId, text){
               }, {
               "type": "postback",
               "title": "مقادير",
-              "payload": recipientId + ",ingredient," +  liens[i],
+              "payload": ',' + recipientId + ",ingredient," +  liens[i],
           },
           {
              "type": "postback",
