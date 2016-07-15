04c4ee104401440feb8fded89d8e2f4d878dfde4
sinchi <ayoub.belghar@gmail.com>
2016-07-15 19:57:54 +0100

    officiel

--- index.js
+++ index.js
@@ -829,11 +829,13 @@ function callSendAPI(messageData) {
   });
 }
 
+
+app.listen((process.env.PORT || 3000));
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid
 // certificate authority.
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-module.exports = app;
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+//
+// module.exports = app;
