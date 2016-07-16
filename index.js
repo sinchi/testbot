9a4403d5f98ee441265114fd61e87247716acb2a
sinchi <ayoub.belghar@gmail.com>
2016-07-16 13:36:27 +0100

    remove menu

--- index.js
+++ index.js
@@ -136,9 +136,9 @@ function rihana(recipientId, text){
 
 // handler receiving message
 app.post('/webhook', function (req, res) {
-  sendMenu();
-  sendGreeting();
-  getStarted();
+//  sendMenu();
+//  sendGreeting();
+//  getStarted();
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
