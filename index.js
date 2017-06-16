eb20d7c354e8d9359cbb37c623bbd03c37d493f6
Ayoub Sinchi <Ayoub Sinchi>
2017-06-16 19:11:12 +0000

    payload

--- index.js
+++ index.js
@@ -104,9 +104,9 @@ function receiveIt(event) {
 
     var messageText = message.text;
     var messageAttachments = message.attachments;
-    //var payload = JSON.parse(message).quick_reply.payload;
-    var payload = event.postback.payload;
-    console.log("payload " + JSON.parse(message));
+    var payload = JSON.parse(message).quick_reply;
+  //  var payload = event.postback.payload;
+    console.log("payload " + payload);
 
   if (messageText) {
 
