97fe1e1c21bd539aa1a18dd0a1facb1eeae12093
Ayoub Sinchi <Ayoub Sinchi>
2017-06-17 18:40:05 +0000

    shopify integration

--- index.js
+++ index.js
@@ -11,16 +11,14 @@ var client = require('./graphql-client');
 const query = client.query((root) => {
   root.add('shop', (shop) => {
     shop.add('name');
-    shop.add('description');
     shop.addConnection('products', {args: {first: 10}}, (product) => {
       product.add('title');
-      product.add('name');
     });
   });
 });
 
-const shopNameAndProductsPromise = client.send(query).then(function(model, data){
-    return model.shop;
+const shopNameAndProductsPromise = client.send(query).then(function(result){
+    return result.model.shop;
   });
 
 
