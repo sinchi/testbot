bcfc943b697af23acd7d2f1eb28c93f6a9ce8eb9
Ayoub Sinchi <Ayoub Sinchi>
2017-06-17 22:02:22 +0000

    shopify integration shopify img

--- index.js
+++ index.js
@@ -333,12 +333,12 @@ function sendGenericMessage(recipientId) {
       for(var i=0; i< edges.length; i++){
         var edge = edges[i];
           console.log(edge);
-         console.log(edge.node.images.edges.node);
+         console.log(edge.node.images.edges);
         elements.push({
           title: edge.node.title,
           subtitle: "T-Shirt of quality, very comfortable and well cut. NOT AVAILABLE IN STORES!",
           item_url: "https://www.teezily.com/hello-summer-oh-yeah?source=store&store=ohyeah-summer",
-          image_url: edge.node.images.edges[0].src,
+          image_url: edge.node.images.edges[0].node.src,
           buttons: [{
             type: "web_url",
             url: "https://www.teezily.com/hello-summer-oh-yeah?source=store&store=ohyeah-summer",
