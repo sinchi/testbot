c99885f2461d43f4195ce5fc7c543851d2abec8d
Ayoub Sinchi <Ayoub Sinchi>
2017-06-17 20:53:00 +0000

    shopify integration shopify

--- index.js
+++ index.js
@@ -283,8 +283,7 @@ function sendGenericMessage(recipientId) {
     uri: 'https://testo-mania.myshopify.com/api/graphql',
     headers: {
       "X-Shopify-Storefront-Access-Token": "3d02750484be7c34eb8d53317b7d1f8a"
-    }
-    method: 'POST',
+    },  
     json: {query: "query {    shop {      name      products (first: 10) {        pageInfo {          hasNextPage          hasPreviousPage        }        edges {          cursor          node {            id            title          }        }      }    }  }"}
 
   }, function (error, response, body) {
