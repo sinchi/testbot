bacdc0a282a72e5406d4f0bc9da94d0a54a94c45
Ayoub Sinchi <Ayoub Sinchi>
2017-06-18 14:24:27 +0000

    show products by watches

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ function watchQuickMessageChoosen(recipientId, page){
                 console.log(image);
                 elements.push({
                   title: edge.title ,
-                  subtitle: stripHTML(edge.body_html),
+                  subtitle: edge.body_html,
                   item_url: "https://testo-mania.myshopify.com/products/"+edge.handle,
                   image_url: (edge.image) ? edge.image.src : 'https://www.iaap-hq.org/global_graphics/default-store-350x350.jpg',
                   buttons: [{
