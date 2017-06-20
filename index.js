0989b6704c282a094904fd2a127750381bdc966d
sinchi <ayoub.belghar@gmail.com>
2017-06-20 11:37:17 +0000

    sort me correct bug

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ function jewelryQuickMessageChoosen(recipientId, page){
       var elements = [];
     //  var sortMe = _.sortBy(JSON.parse(body).products, 'created_at');
       console.log("SOORT ME SORT ME");
-      console.log(sortMe);
+      //console.log(sortMe);
       var edges = JSON.parse(body).products;
       for(var i=0; i< edges.length; i++){
         var edge = edges[i] ;
